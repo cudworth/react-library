4e1eab3b22f752bfa88c9d713510853af3deb850
Austin Cudworth <austincudworth@gmail.com>
2020-09-09 21:16:47 -0700

    Initialize React components for library project

---
+++ src/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return <div className="Header">Hello World from the Header!</div>;
+}
+
+export default Header;
---
+++ src/InputForm/InputForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './InputForm.css';
+
+function InputForm() {
+  return <div className="InputForm">Hello World from an Input Form!</div>;
+}
+
+export default InputForm;
